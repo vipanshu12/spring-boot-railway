96f1b5c0b207540091b4221b0ad1a38525e61e59
vipanshu12 <vgupta48030@gmail.com>
2025-06-09 23:20:52 +0530

    Update HomeController.java

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HomeController {
@@ -9,5 +10,12 @@ public class HomeController {
     @GetMapping
     public String homeControllerHandler() {
         return "this is home controller";
+    }
+
+    // This prevents 404/502 errors for favicon.ico requests
+    @GetMapping("/favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
+        // Do nothing (satisfies the browser's request silently)
     }
 }
